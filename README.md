e1de56415b513b64bb654d2511cca8353ecde092
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-03-17 06:42:44 +0000

    Script to detect unreferenced commits
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # detect-removed-commits
 List git commits stored in git objects that are not connected to any revision (squashed commits for example)
+
+- Run `./detect-removed-commits.sh` in git repo path to see cmmits that were removed/squashed etc.
+
---
+++ detect-removed-commits.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+declare -A glog
+declare -A gfile
+#for f in `find .git/objects/??/ -type f | sed 's/\(.*\)\/\([[:xdigit:]]\{2\}\)\/\([[:xdigit:]]\+\)$/\2\3/g'`
+for f in `git cat-file --batch-all-objects --buffer --batch-check | grep ' commit ' | awk '{print $1}'`
+do
+  gfile[$f]=1
+done
+for f in `git rev-list --all --remotes`
+do
+  glog[$f]=1
+done
+missing=''
+for f in "${!gfile[@]}"
+do
+  got=${glog[$f]}
+  if [ ! "$got" = "1" ]
+  then
+    if [ -z "${missing}" ]
+    then
+      missing="$f"
+    else
+      missing="${missing} ${f}"
+    fi
+  fi
+done
+if [ ! -z "${missing}" ]
+then
+  echo "${missing}"
+fi
